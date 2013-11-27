f050ca0b38cc00c6ccff87fe0f5f3ec9b4e90a80
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2013-11-27 22:46:39 +0100

    Oprava

--- src/abstract/ADrawable.cpp
+++ src/abstract/ADrawable.cpp
@@ -30,11 +30,8 @@ void ADrawable::setVAO()
 
 void ADrawable::setVB()
 {
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &_vertexBufferID);
-    // The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferID);
-    // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
     
     if (_vertexBufferID == -1) std::cout<<"_vertexBufferID still -1?"<<std::endl;
@@ -50,27 +47,26 @@ void ADrawable::setColor()
 
 void ADrawable::draw()
 {
-    // 1rst attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferID);
     glVertexAttribPointer(
-       0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-       3,                  // size
-       GL_FLOAT,           // type
-       GL_FALSE,           // normalized?
-       0,                  // stride
-       (void*)0            // array buffer offset
+       0,
+       3,
+       GL_FLOAT,
+       GL_FALSE,
+       0,
+       (void*)0
     );
     
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, _colorbuffer);
     glVertexAttribPointer(
-        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        3,                                // size
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        0,
+        (void*)0
     );
 
 
@@ -78,8 +74,7 @@ void ADrawable::draw()
     //glUniform3fv(10, 1, glm::value_ptr(glm::vec3(1.f,0.f,1.f)));
     
     
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLES, 0, 12*3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+    glDrawArrays(GL_TRIANGLES, 0, 12*3);
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
